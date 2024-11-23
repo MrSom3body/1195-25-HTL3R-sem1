928b0c724362ef32688180a912fc8e445167d004
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-11-23 19:39:00 +0100

    02_Enum: add simple test cases

--- 02_Enum/src/wordcount/WordCountTests.java
+++ 02_Enum/src/wordcount/WordCountTests.java
@@ -1,5 +1,15 @@
 package wordcount;
 
-public class WordCountTests {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WordCountTests {
+    @Test
+    public void testCountSimple() {
+        WordCount wordCount = new WordCount();
+        assertEquals(0, wordCount.count(""));
+        assertEquals(0, wordCount.count("1337"));
+        assertEquals(3, wordCount.count("Das sind 3 Wörter"));
+    }
 }

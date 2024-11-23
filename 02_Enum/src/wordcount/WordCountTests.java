8e0331a643ca362a3b3ed16f1253385650d98830
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-11-23 20:27:39 +0100

    02_Enum: add big test case with the provided file

--- 02_Enum/src/wordcount/WordCountTests.java
+++ 02_Enum/src/wordcount/WordCountTests.java
@@ -2,6 +2,11 @@ package wordcount;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WordCountTests {
@@ -17,5 +22,21 @@ public class WordCountTests {
     public void testCountComplex() {
         WordCount wordCount = new WordCount();
         assertEquals(9, wordCount.count("Das ist ein String mit Sonderzeichen, Zahlen (123) und 9 Wörtern!"));
+    }
+
+    @Test
+    void testCountBig() throws IOException {
+        try (
+                BufferedReader in = Files.newBufferedReader(Path.of("resources/crsto12.html"))
+        ) {
+            StringBuilder content = new StringBuilder();
+            WordCount wordCount = new WordCount();
+            String line;
+            while ((line = in.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            // The file contains 482515 words (Source: Luka)
+            assertEquals(482515, wordCount.count(content.toString()));
+        }
     }
 }

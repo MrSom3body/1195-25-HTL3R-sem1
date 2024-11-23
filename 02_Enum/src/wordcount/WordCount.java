dda31143cd93f79f11e1a6cf81494debae23739e
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-11-23 20:28:17 +0100

    02_Enum: add wordcount class with inital code

---
+++ 02_Enum/src/wordcount/WordCount.java
@@ -0,0 +1,41 @@
+package wordcount;
+
+public class WordCount {
+    enum State {
+        INWORD {
+            @Override
+            State handleChar(char c, WordCount context) {
+                if (!Character.isLetter(c)) {
+                    context.counter++;
+                    return NOWORD;
+                } else {
+                    return INWORD;
+                }
+            }
+        },
+
+        NOWORD {
+            @Override
+            State handleChar(char c, WordCount context) {
+                if (Character.isLetter(c)) {
+                    return INWORD;
+                } else {
+                    return NOWORD;
+                }
+            }
+        };
+
+        abstract State handleChar(char c, WordCount context);
+    }
+
+    int counter;
+
+    public int count(String text) {
+        State state = State.NOWORD;
+        counter = 0;
+        for (char c : (text += " ").toCharArray()) {
+            state = state.handleChar(c, this);
+        }
+        return counter;
+    }
+}

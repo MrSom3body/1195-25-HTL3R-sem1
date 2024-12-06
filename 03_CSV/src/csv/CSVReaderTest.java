37f10ba260e0355ddde776e711b45aef9194043b
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-12-06 11:04:01 +0100

    03_CSV: add simple test case

---
+++ 03_CSV/src/csv/CSVReaderTest.java
@@ -0,0 +1,15 @@
+package csv;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class CSVReaderTest {
+    @Test
+    public void simpleTestRead() {
+        CSVReader reader = new CSVReader(',', '"', true);
+        assertArrayEquals(new String[]{"one", "two", " three"}, reader.read("one,two, three"));
+        assertArrayEquals(new String[]{"one", "", "three"}, reader.read("one,,three"));
+        assertArrayEquals(new String[]{"bomboclad1 bomboclad2"}, reader.read("bomboclad1 bomboclad2"));
+    }
+}

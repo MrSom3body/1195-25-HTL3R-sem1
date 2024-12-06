928f4aa2632b52a88999295497f9650d60351b51
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-12-06 18:25:31 +0100

    03_CSV: handle quoted text in read

--- 03_CSV/src/csv/CSVReader.java
+++ 03_CSV/src/csv/CSVReader.java
@@ -17,9 +17,34 @@ public class CSVReader {
                     context.words.add(context.word);
                     context.word = "";
                     return READING;
+                } else if (ch == context.doublequote) {
+                    return QUOTED;
                 } else {
                     context.word += ch;
                     return this;
+                }
+            }
+        }, QUOTED {
+            State handleChar(char ch, CSVReader context) {
+                if (ch == '\0') {
+                    throw new IllegalArgumentException("Open quote after: " + ch);
+                } else if (ch == context.doublequote) {
+                    context.words.add(context.word);
+                    context.word = "";
+                    return DISABLE_QUOTED;
+                } else {
+                    context.word += ch;
+                    return this;
+                }
+            }
+        }, DISABLE_QUOTED {
+            State handleChar(char ch, CSVReader context) {
+                if (ch == '\0') {
+                    return READING;
+                } else if (ch == context.delimeter) {
+                    return READING;
+                } else {
+                    throw new IllegalArgumentException("Unexpected token after closing quote: " + ch);
                 }
             }
         };

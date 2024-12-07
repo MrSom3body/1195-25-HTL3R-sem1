c90490071f69db5ef05f71773c6044f587efa111
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-12-07 15:59:13 +0100

    03_CSV: simplify next() method in CSVFileReader

--- 03_CSV/src/csv/CSVFileReader.java
+++ 03_CSV/src/csv/CSVFileReader.java
@@ -16,11 +16,7 @@ public class CSVFileReader implements AutoCloseable {
 
     public String[] next() throws IOException {
         String fileLine = fileReader.readLine();
-        if (fileLine == null) {
-            return new String[0];
-        } else {
-            return reader.read(fileLine);
-        }
+        return fileLine != null ? reader.read(fileLine) : null;
     }
 
     @Override

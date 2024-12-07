40ba4b8bcac5c13719360484230b2602ccc34ee8
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-12-07 14:06:56 +0100

    03_CSV: add test case for CSVFileReader

---
+++ 03_CSV/src/csv/CSVFileReaderTest.java
@@ -0,0 +1,24 @@
+package csv;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class CSVFileReaderTest {
+    @Test
+    public void testNext() throws IOException {
+        CSVFileReader reader = new CSVFileReader("resources/adjacency.csv", ';', '"', true);
+        assertArrayEquals(new String[]{"", "A", "B", "C", "D", "E", "F", "G", "H"}, reader.next());
+        assertArrayEquals(new String[]{"A", "", "4", "7", "8", "", "", "", ""}, reader.next());
+        assertArrayEquals(new String[]{"B", "4", "", "", "", "5", "3", "", ""}, reader.next());
+        assertArrayEquals(new String[]{"C", "7", "", "", "2", "", "", "1", ""}, reader.next());
+        assertArrayEquals(new String[]{"D", "8", "5", "2", "", "1", "", "2", ""}, reader.next());
+        assertArrayEquals(new String[]{"E", "", "3", "", "1", "", "1", "", "5"}, reader.next());
+        assertArrayEquals(new String[]{"F", "", "3", "", "", "1", "", "", "1"}, reader.next());
+        assertArrayEquals(new String[]{"G", "", "", "1", "2", "", "", "", "1"}, reader.next());
+        assertArrayEquals(new String[]{"H", "", "", "", "", "5", "1", "1", ""}, reader.next());
+        assertArrayEquals(new String[0], reader.next());
+    }
+}

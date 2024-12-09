620ee6d2f680de86ac6d67815474aa37c57dbb48
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-12-09 12:05:59 +0100

    add a nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Nix flake for software development";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          # jetbrains.idea-ultimate
+          jdt-language-server
+
+          jdk
+        ];
+
+        shellHook = ''
+          echo "Java Version:"
+          java -version
+        '';
+      };
+  };
+}

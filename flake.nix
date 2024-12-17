a6aeb4057a6a626ab026c0ee1053b409b1ab4f84
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2024-12-17 12:24:00 +0100

    add .gitignore and devenv for python

--- flake.nix
+++ flake.nix
@@ -1,31 +1,79 @@
 {
-  description = "Nix flake for software development";
-
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixpkgs-python = {
+      url = "github:cachix/nixpkgs-python";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in
-      pkgs.mkShell {
-        packages = with pkgs; [
-          # jetbrains.idea-ultimate
-          jdt-language-server
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+      devenv-test = self.devShells.${system}.default.config.test;
+    });
 
-          jdk8
-        ];
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = with pkgs; [
+                git # duh
+                helix # the best code editor
 
-        shellHook = ''
-          echo "Java Version:"
-          java -version
-        '';
-      };
+                jdt-language-server
+                jdk8
+              ];
+
+              languages.python = {
+                enable = true;
+                version = "3.12";
+                poetry = {
+                  enable = true;
+                  activate.enable = true;
+                  install.enable = true;
+                };
+              };
+
+              enterShell = ''
+                git --version
+                hx --version
+              '';
+
+              pre-commit.hooks = {
+                autoflake.enable = true;
+                ruff.enable = true;
+              };
+            }
+          ];
+        };
+      });
   };
 }

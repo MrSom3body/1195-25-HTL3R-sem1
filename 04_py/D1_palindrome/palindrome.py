57c8a5091933cbd326ce1ae18cf7afc3b668c6ab
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-07 19:24:04 +0100

    04_py: add test cases for is_palindrome_sentence()

--- 04_py/D1_palindrome/palindrome.py
+++ 04_py/D1_palindrome/palindrome.py
@@ -13,7 +13,14 @@ def is_palindrome(s: str) -> bool:
     return s == reverse
 
 
+def is_palindrome_sentence(s: str)-> bool:
+    return False
+
 if __name__ == "__main__":
     print("Expected: True, Actual:", is_palindrome("anna"))
     print("Expected: False, Actual:", is_palindrome("hello"))
     print("Expected: True, Actual:", is_palindrome("racecar"))
+
+    print("Expected: True, Actual:", is_palindrome_sentence("Was it a car or a cat I saw?"))
+    print("Expected: False, Actual:", is_palindrome_sentence("This is not a palindrome."))
+    print("Expected: True, Actual:", is_palindrome_sentence("A man, a plan, a canal, Panama!"))

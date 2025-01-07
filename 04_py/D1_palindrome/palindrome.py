266063ede3f3947a30ee6bfb9484cf360c19fa76
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-07 19:11:27 +0100

    04_py: add test cases for is_palindrome()

---
+++ 04_py/D1_palindrome/palindrome.py
@@ -0,0 +1,11 @@
+__author__ = "Karun Sandhu"
+
+
+def is_palindrome(s: str) -> bool:
+    return False
+
+
+if __name__ == "__main__":
+    print("Expected: True, Actual:", is_palindrome("anna"))
+    print("Expected: False, Actual:", is_palindrome("hello"))
+    print("Expected: True, Actual:", is_palindrome("racecar"))

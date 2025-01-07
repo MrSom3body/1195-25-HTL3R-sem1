7cdf90891192339f4fc613c2e4db50a1a5378c57
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-07 21:20:38 +0100

    04_py: add test cases for collatz_sequence()

--- 04_py/D2_collatz/collatz.py
+++ 04_py/D2_collatz/collatz.py
@@ -8,7 +8,51 @@ def collatz(n: int) -> int:
         return 3 * n + 1
 
 
+def collatz_sequence(number: int) -> list[int]:
+    return []
+
+
 if __name__ == "__main__":
     assert collatz(1) == 4
     assert collatz(5) == 16
     assert collatz(8) == 4
+
+    assert collatz_sequence(14) == [
+        14,
+        7,
+        22,
+        11,
+        34,
+        17,
+        52,
+        26,
+        13,
+        40,
+        20,
+        10,
+        5,
+        16,
+        8,
+        4,
+        2,
+        1,
+    ]
+    assert collatz_sequence(7) == [
+        7,
+        22,
+        11,
+        34,
+        17,
+        52,
+        26,
+        13,
+        40,
+        20,
+        10,
+        5,
+        16,
+        8,
+        4,
+        2,
+        1,
+    ]

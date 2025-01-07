fb174a9350adeb5edb540b6b0bdc6131d81f5c4c
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-07 19:44:29 +0100

    04_py: fix formatting

--- 04_py/D1_palindrome/palindrome.py
+++ 04_py/D1_palindrome/palindrome.py
@@ -1,5 +1,6 @@
 __author__ = "Karun Sandhu"
 
+
 def is_palindrome(s: str) -> bool:
     """
     :param s: string to check if it is a palindrom
@@ -12,23 +13,33 @@ def is_palindrome(s: str) -> bool:
     return s == reverse
 
 
-def is_palindrome_sentence(s: str)-> bool:
+def is_palindrome_sentence(s: str) -> bool:
     """
     :param s: string of a sentence to check if it is a palindrom
     :return: True if s is a palindrom, False otherwise
     """
     filtered = ""
     for ch in s:
-        if ch.isalnum(): 
+        if ch.isalnum():
             filtered += ch
 
     return is_palindrome(filtered.lower())
+
 
 if __name__ == "__main__":
     print("Expected: True, Actual:", is_palindrome("anna"))
     print("Expected: False, Actual:", is_palindrome("hello"))
     print("Expected: True, Actual:", is_palindrome("racecar"))
 
-    print("Expected: True, Actual:", is_palindrome_sentence("Was it a car or a cat I saw?"))
-    print("Expected: False, Actual:", is_palindrome_sentence("This is not a palindrome."))
-    print("Expected: True, Actual:", is_palindrome_sentence("A man, a plan, a canal, Panama!"))
+    print(
+        "Expected: True, Actual:",
+        is_palindrome_sentence("Was it a car or a cat I saw?"),
+    )
+    print(
+        "Expected: False, Actual:",
+        is_palindrome_sentence("This is not a palindrome."),
+    )
+    print(
+        "Expected: True, Actual:",
+        is_palindrome_sentence("A man, a plan, a canal, Panama!"),
+    )

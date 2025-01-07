5b09ce0da16dc19a770235d23e5148136a9623ae
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-07 22:05:19 +0100

    04_py: add McCarthy list with 200 elements

--- 04_py/D3_rekursiv/rekursiv.py
+++ 04_py/D3_rekursiv/rekursiv.py
@@ -7,3 +7,9 @@ def M(n: int) -> int:
         return M(M(n + 11))
     else:
         return n - 10
+
+
+if __name__ == "__main__":
+    # The McCarthy-91 function is special because every input from 0-101 will return 91
+    m_list = [M(i) for i in range(1, 201)]
+    print(m_list)

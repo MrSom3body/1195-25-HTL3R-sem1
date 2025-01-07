ee2b28bbdbafa8ac4af7006cafef425063355b2c
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-07 22:08:49 +0100

    04_py: measure time for McCarthy

--- 04_py/D3_rekursiv/rekursiv.py
+++ 04_py/D3_rekursiv/rekursiv.py
@@ -1,3 +1,6 @@
+from time import time
+
+
 def M(n: int) -> int:
     """
     :param n: the input integer to run through the McCarthy-91 function
@@ -11,6 +14,9 @@ def M(n: int) -> int:
 
 if __name__ == "__main__":
     # The McCarthy-91 function is special because every input from 0-101 will return 91
+    t_start = time()
     m_list = [M(i) for i in range(1, 201)]
     m_dict = {i: M(i) for i in range(1, 201)}
-    print(m_dict)
+    t_end = time()
+    print(f"Time elapsed: {t_end - t_start:.6f}s")
+    # takes 0.000736s on my machine

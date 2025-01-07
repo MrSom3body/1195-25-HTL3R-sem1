e5b913c6f7742cb02de79da4021051594133e438
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-07 20:13:19 +0100

    04_py: add to_base() implementation

--- 04_py/D1_palindrome/palindrome.py
+++ 04_py/D1_palindrome/palindrome.py
@@ -47,6 +47,23 @@ def palindrome_product(x: int) -> int:
     return largest_palindrome
 
 
+def to_base(number: int, base: int) -> str:
+    """
+    :param number: Zahl im 10er-Syste,
+    :param base: Zielsystem (maximal 36)
+    :return: Zahl im Zielsystem als String
+    >>> to_base(1234, 16)
+    '4D2'
+    """
+    new_base = ""
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVXYZ"
+    while number != 0:
+        new_base = digits[number % base] + new_base
+        number = number // base
+
+    return new_base
+
+
 if __name__ == "__main__":
     print("Expected: True, Actual:", is_palindrome("anna"))
     print("Expected: False, Actual:", is_palindrome("hello"))
@@ -68,3 +85,7 @@ if __name__ == "__main__":
     print("Expected: 906609, Actual:", palindrome_product(1000000))  # 913 x 993
     print("Expected: 793397, Actual:", palindrome_product(800000))  # 869 x 913
     print("Expected: 595595, Actual:", palindrome_product(600000))  # 935 x 637
+
+    import doctest
+
+    _ = doctest.testmod()

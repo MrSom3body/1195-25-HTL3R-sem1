2465b5d0cc11e7ec350eca7d26ec5f3d5a4cf13d
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-01-08 07:40:43 +0100

    04_py: also return the recursion depth for M

--- 04_py/D3_rekursiv/rekursiv.py
+++ 04_py/D3_rekursiv/rekursiv.py
@@ -1,15 +1,18 @@
 from time import time
+from pprint import pprint
 
 
-def M(n: int) -> int:
+def M(n: int, rec: int = 0) -> tuple[int, int]:
     """
     :param n: the input integer to run through the McCarthy-91 function
-    :return: the computed result
+    :return: the computed result and the recursion depth
     """
     if n <= 100:
-        return M(M(n + 11))
+        m1, rec = M(n + 11, rec + 1)
+        m2, rec = M(m1, rec + 1)
+        return m2, rec
     else:
-        return n - 10
+        return n - 10, rec
 
 
 if __name__ == "__main__":
@@ -19,4 +22,6 @@ if __name__ == "__main__":
     m_dict = {i: M(i) for i in range(1, 201)}
     t_end = time()
     print(f"Time elapsed: {t_end - t_start:.6f}s")
+    pprint(m_dict)
+    # the recursion depth for 1 was the highest
     # takes 0.000736s on my machine

f07106e274cd25f8c0cfc13f23c082d0a18379c9
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-02-18 21:02:16 +0100

    05_kasiski: add alldist function to Kasiski class

--- 05_kasiski/kasiski.py
+++ 05_kasiski/kasiski.py
@@ -21,6 +21,26 @@ class Kasiski:
             if text[i : i + len(substring)] == substring
         ]
 
+    def alldist(self, text: str, substring: str) -> set[int]:
+        """
+        Calculate all distances between all occurences of substring in text.
+        :param text: the text to look in
+        :param substring: the substring to look for
+        :return: a list with the distances
+
+        >>> k = Kasiski()
+        >>> k.alldist("heissajuchei, ein ei", "ei")
+        {4, 8, 9, 13, 17}
+        >>> k.alldist("heissajuchei, ein ei", "hai")
+        set()
+        """
+        dist: set[int] = set()
+        pos = self.allpos(text, substring)
+        for i in range(len(pos)):
+            for j in range(i + 1, len(pos)):
+                dist.add(pos[j] - pos[i])
+        return dist
+
 
 if __name__ == "__main__":
     import doctest

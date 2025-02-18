7db9f4adc532b51520deea12a068db18b6f31346
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-02-18 21:15:26 +0100

    05_kasiski: add dist_n_tuple function to Kasiski class

--- 05_kasiski/kasiski.py
+++ 05_kasiski/kasiski.py
@@ -41,6 +41,34 @@ class Kasiski:
                 dist.add(pos[j] - pos[i])
         return dist
 
+    def dist_n_tuple(self, text: str, length: int) -> set[tuple[str, int]]:
+        """
+        Checks all substrings of the text with the provided length and returns
+        a set with all distances of all repetitions of the substring.
+        :param text: the text to look in
+        :param length: the lenght of the substrings to look for
+        :return: a set containig tuples containing the substring and the count
+        
+        >>> k = Kasiski()
+        >>> k.dist_n_tuple("heissajuchei", 2) == {('ei', 9), ('he', 9)}
+        True
+        >>> k.dist_n_tuple("heissajuchei", 3) == {('hei', 9)}
+        True
+        >>> k.dist_n_tuple("heissajuchei", 4) == set()
+        True
+        >>> k.dist_n_tuple("heissajucheieinei", 2) == \
+        {('ei', 5), ('ei', 14), ('ei', 3), ('ei', 9), ('ei', 11), ('he', 9), ('ei', 2)}
+        True
+        """
+        dist_n: set[tuple[str, int]] = set()
+        for i in range(len(text) - length + 1):
+            substring = text[i : i + length]
+            pos = self.allpos(text, substring)
+            if len(pos) > 1:
+                for dist in self.alldist(text, substring):
+                    dist_n.add((substring, dist))
+        return dist_n
+
 
 if __name__ == "__main__":
     import doctest

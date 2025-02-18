5188cf46eb482724c31e06851b93c666cc89898d
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-02-18 20:29:23 +0100

    05_kaiski: add encrypt function to Vigenere class

--- 05_kaiski/vigenere.py
+++ 05_kaiski/vigenere.py
@@ -15,6 +15,35 @@ class Vigenere:
         else:
             return self.check_key(fallback_key)
 
+    def encrypt(self, plaintext: str, key: str | None = None) -> str:
+        """
+        Encrypts a plaintext with the Vigenere cipher.
+        :param plaintext: the plaintext to be encrypted
+        :param key: the key to be used for the encryption
+        :return: the encrypted plaintext
+
+        >>> vigenere = Vigenere()
+        >>> vigenere.encrypt("hello", "abcde")
+        'hfnos'
+
+        >>> vigenere.encrypt("attackatdawn", "lemon")
+        'lxfopvefrnhr'
+
+        >>> vigenere.encrypt("", "abcd")
+        ''
+        >>> vigenere.encrypt("hello", "ABCDE d 1 213 12")
+        Traceback (most recent call last):
+        ...
+        ValueError: The key must only contain letters: ABCDE d 1 213 12
+        """
+        key = self.check_key(key, self.key)
+        encrypted = ""
+
+        for index, ch in enumerate(plaintext):
+            encrypted += self.caesar.encrypt(ch, key[index % len(key)])
+
+        return encrypted
+
 
 if __name__ == "__main__":
     import doctest
